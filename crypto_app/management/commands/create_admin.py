5539226fb900eba435511c5070b81b3f6f87e243
fathima661 <fathimashaji66@gmail.com>
2026-04-20 09:16:36 +0530

    fix: admin setup for neon migration

--- crypto_app/management/commands/create_admin.py
+++ crypto_app/management/commands/create_admin.py
@@ -1,20 +1,18 @@
-'''
 from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
+from crypto_app.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        User = get_user_model()
-
         email = "admin@gmail.com"
         password = "Admin@2001"
 
-        if not User.objects.filter(email=email).exists():
-            User.objects.create_superuser(
-                email=email,
-                password=password
-            )
-            self.stdout.write("Admin created")
-        else:
+        if User.objects.filter(email=email).exists():
             self.stdout.write("Admin already exists")
-'''
+            return
+
+        user = User.objects.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
+        self.stdout.write("Admin created successfully")
